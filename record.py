2142bae50daf5d8d6f24b228c832a0688bb5daa5
tblomqvi <torsten.blomqvist@abo.fi>
2017-02-25 15:08:52 +0200

    fix bug and small changes

--- record.py
+++ record.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import sys, getopt
 import threading
-
+from threading import Thread, Lock
 import RPi.GPIO as GPIO
 import os
 import time
@@ -11,19 +11,40 @@ import time
 nCameras = 0
 cap = [0]*(nCameras+1)
 out = [0]*(nCameras+1)
-frame = [0]*(nCameras+1)
 	
 picturecount = 0
 videocount = 0
 
-def do_every(interval, worker_func):
+fps = 8
+width = 640
+height = 480
+
+mutex = Lock()
+
+def readVideo(cap):
+	frame = [0]*(nCameras+1)
+	for i in range(0,nCameras):
+		frame[i] = cap[i].read()
+	return frame
+
+def writeVideo(frame):
+	global out
+	for i in range(0,nCameras):
+		out[i].write(frame[i])
+		
+def recordVideo(fps):
+	global cap
 	if(GPIO.input(4)):
-		threading.Timer(interval, do_every, [interval, worker_func]).start()
-		worker_func()
+		t = threading.Timer(1.0/fps, recordVideo,[fps])
+		t.daemon = True
+		t.start()
+		frame = readVideo(cap)
+		mutex.acquire()
+		writeVideo(frame)
+		mutex.release()	
 	else:
 		new_VideoWriters()
 		recording = False
-	
 	
 def take_picture():
 	global frame, cap, nCameras, picturecount
@@ -32,28 +53,18 @@ def take_picture():
 	for i in range(0,nCameras):
 		cv2.imwrite('picture'+str(picturecount)+'_'+str(i)+'.png', frame[i])
 				
-def record_frame():
-	global frame, cap, out
-	# read frames
-	for i in range(0,nCameras):
-		frame[i] = cap[i].read()
-				
-	# write the frames
-	for i in range(0,nCameras):
-		out[i].write(frame[i])
-
 
 def new_VideoWriters():
 	global out, videocount
 	for i in range(0,nCameras):
 		out[i] = cv2.VideoWriter('video'+str(videocount)+'_'+str(i)+'.avi', fourcc, fps, (width,height))
+		print ('New video')
 	videocount += 1
 
 def main(argv):
 
 	global nCameras
-	global cap, out, frame, picturecount, fourcc
-	global fps, width, height
+	global cap, picturecount, fourcc
 	
 	try:
 		opts, args = getopt.getopt(argv,"hn:")
@@ -65,7 +76,7 @@ def main(argv):
 			print 'record.py -n <number_of_cameras>'
 			sys.exit()
 		elif opt in ("-n"):
-			nCameras = arg
+			nCameras = int(arg)
 	
 	if nCameras == 0:
 		print 'record.py -n <number_of_cameras>'
@@ -81,11 +92,8 @@ def main(argv):
 
 	#Start/stop recording input-pin
 	GPIO.setup(4,GPIO.IN)
+	print ('GPIO set up')
 
-
-	fps = 10
-	width = 640
-	height = 480
 	
 
 	# Create VideoCapture objects
@@ -95,22 +103,23 @@ def main(argv):
 	# Define the codec and create VideoWriter objects
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	new_VideoWriters()
-	
 	recording = False
 
 	while(1):
 		
 		#if take picture
-		if(GPIO.input(2)):
+		if(GPIO.input(2)):	
+			print 'Take picture'
 			take_picture()
 			picturecount += 1
 			time.sleep(1)
 			
 				
 		#if take video
-		if(GPIO.input(4)):
+		if(GPIO.input(4)):	
 			recording = True
-			do_every(1/fps, record_frame)
+			print 'Start recording'
+			recordVideo(fps)
 			while(recording):
 				pass
 			
